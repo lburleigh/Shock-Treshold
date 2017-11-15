6885b6f9b68f79ca4db9f77200a058eb794eccff
Lauryn Burleigh <lburle2@lsu.edu>
2017-11-15 16:11:50 -0600

    threshold now raises level once uncomfortable

--- threshold.m
+++ threshold.m
@@ -1,8 +1,8 @@
 %% Shock Threshold task
 % threshold
 %
-%  by L Burleigh (lburleigh92@gmail.com)
-%
+% by L Burleigh (lburleigh92@gmail.com)
+% Edited by Xinrui Jiang (xj1992ch@gmail.com)
 
 
 addpath(genpath('Scripts'));
@@ -28,7 +28,7 @@ FlushEvents;
 %% Event of Interest
 
 go=true;
-
+n = 0;
 while go;
     DrawFormattedText(w, sprintf('Prepare for a MILD SHOCK'), 'center','center', 0);
     Flip1 = Screen('Flip', w);
@@ -36,71 +36,44 @@ while go;
     DaqDOut(daq,0,EmShock);
     Screen('Flip',w, Flip1 + 4 - slack) ;
     DaqDOut(daq,0,EmOFF);
-    DrawFormattedText(w, sprintf('Was the shock...\n\n1. Too low/not uncomfortable\n\n2. Too high/painful\n\n3. Uncomfortable but not painful'), 'center','center', 0);
+    DrawFormattedText(w, sprintf('Was the shock...\n\nIndex Finger: Too low/not uncomfortable\n\nMiddle Finger: Too high/painful\n\nRing Finger: Uncomfortable but not painful'), 'center','center', 0);
     Screen('Flip',w);
     KbWait
     [keyIsDown, endSecs, keyCode] = KbCheck(-1);
     if any(keyCode(KB.Resp1))
-        UserResp = 1;
-        DrawFormattedText(w, sprintf('Shock is too low'), 'center','center', 0);
+        DrawFormattedText(w, sprintf('Shock was too low'), 'center','center', 0);
         Screen('Flip', w);
         WaitSecs(2)
         DrawFormattedText(w, sprintf('Waiting for Experimenter'), 'center','center', 0);
         Screen('Flip', w);
         KbWait
-        keyCode(KB.SpaceBar)
-        go = true;
     elseif any(keyCode(KB.Resp2))
-        UserResp = 2;
-        DrawFormattedText(w, sprintf('Shock is too high'), 'center','center', 0);
+        DrawFormattedText(w, sprintf('Shock was too high'), 'center','center', 0);
         Screen('Flip', w);
         WaitSecs(2)
         DrawFormattedText(w, sprintf('Waiting for Experimenter'), 'center','center', 0);
         Screen('Flip', w);
         KbWait
-        keyCode(KB.SpaceBar)
-        go = true;
     elseif any(keyCode(KB.Resp3))
-        UserResp = 3;
-        DrawFormattedText(w, sprintf('Proper shock intensity achieved'), 'center','center', 0);
-        Screen('Flip', w);
-        WaitSecs(2);
-        DrawFormattedText(w, sprintf('Prepare for one more shock at a higher intensity'), 'center','center', 0);
-        Flip2 = Screen('Flip', w);
-        Screen('Flip',w, Flip2 + 3.995 - slack);
-        DaqDOut(daq,0,EmShock);
-        Screen('Flip',w, Flip2 + 4 - slack) ;
-        DaqDOut(daq,0,EmOFF);
-        DrawFormattedText(w, sprintf('Was the shock...\n\n1. Too low/not uncomfortable\n\n2. Too high/painful\n\n3. Uncomfortable but not painful'), 'center','center', 0);
-        Screen('Flip',w);
-        KbWait
-        [keyIsDown, endSecs, keyCode] = KbCheck(-1);
-        if any(keyCode(KB.Resp1))
-            UserResp = 1;
-            DrawFormattedText(w, sprintf('Shock is too low'), 'center','center', 0);
+        n = n+1;
+        if n == 2;
+            DrawFormattedText(w, sprintf('Shock was uncomfortable but not painful'), 'center','center', 0);
             Screen('Flip', w);
-            WaitSecs(2)
-            DrawFormattedText(w, sprintf('Waiting for Experimenter'), 'center','center', 0);
-            Screen('Flip', w);
-            KbWait
-            keyCode(KB.SpaceBar)
-            go = true;
-        elseif any(keyCode(KB.Resp2))
-            UserResp = 2;
-            DrawFormattedText(w, sprintf('Shock is too high'), 'center','center', 0);
-            Screen('Flip', w);
-            WaitSecs(2)
-            DrawFormattedText(w, sprintf('Waiting for Experimenter'), 'center','center', 0);
-            Screen('Flip', w);
-            KbWait
-            keyCode(KB.SpaceBar)
-            go = true;
-        elseif any(keyCode(KB.Resp3))
-            UserResp = 3;
+            WaitSecs(2);
             DrawFormattedText(w, sprintf('Proper shock intensity achieved'), 'center','center', 0);
             Screen('Flip', w);
-            keyCode(KB.SpaceBar)
+            WaitSecs(2);
             go = false;
+        elseif n == 1;
+            DrawFormattedText(w, sprintf('Shock was uncomfortable but not painful'), 'center','center', 0);
+            Screen('Flip', w);
+            WaitSecs(2);
+            DrawFormattedText(w, sprintf('Prepare for one more shock at a higher intensity'), 'center','center', 0);
+            Screen('Flip', w);
+            WaitSecs(2)
+            DrawFormattedText(w, sprintf('Waiting for Experimenter'), 'center','center', 0);
+            Screen('Flip', w);
+            KbWait
         end
     end
 end    
